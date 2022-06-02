71fe00460fc831a73de28a3f1d4caa5d36d84c5b
Nagao-Kai-0902 <87113050+Nagao-Kai-0902@users.noreply.github.com>
2022-06-02 13:56:50 +0900

    バリデーション追加までのコミット

--- src/main/java/com/example/demo/Controller/StaffController.java
+++ src/main/java/com/example/demo/Controller/StaffController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.Service.StaffDataService;
 import com.example.demo.model.User;
@@ -44,12 +47,26 @@ public class StaffController {
 	}
 
 	@RequestMapping(path = "new", method = RequestMethod.GET)
-	public String newStaff() {
+	public String newStaff(Model model) {
+		if (!model.containsAttribute("user")) {
+			model.addAttribute("user", new User());
+		}
 		return "new";
 	}
 
 	@RequestMapping(path = "new/registration", method = RequestMethod.POST)
-	public String insert(@ModelAttribute User user) {
+	public String insert(@ModelAttribute("user") @Validated User user, BindingResult result, Model model,
+			RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors()) {
+//			List<String> errorList = new ArrayList<String>();
+//			for (ObjectError error : result.getAllErrors()) {
+//				errorList.add(error.getDefaultMessage());
+//			}
+//			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.test1Form", result);
+//			model.addAttribute("validationError", errorList);
+			return "new";
+		}
 
 		service.newEmployee(user);
 
@@ -80,26 +97,39 @@ public class StaffController {
 	}
 
 	@GetMapping(value = "/edit/{staff_code}")
-	public String displayEdit(Model model, @PathVariable("staff_code") String staffCode) {
+	public String displayEdit(Model model, @PathVariable("staff_code") String staff_code) {
 
-		User user = service.selectOne(staffCode);
+		User user = service.selectOne(staff_code);
 		model.addAttribute("user", user);
 
 		return "/edit";
 	}
 
 	@RequestMapping(path = "/update", method = RequestMethod.POST)
-	public String update(Model model, User user, @RequestParam String staff_code_before) {
+	public String update(@ModelAttribute("user") @Validated User user, BindingResult result, Model model,
+			RedirectAttributes redirectAttributes, @PathVariable("staff_code_before") @RequestParam String staff_code_before,
+			String staff_code) {
 
+		if (result.hasErrors()) {
+//			List<String> errorList = new ArrayList<String>();
+//			for (ObjectError error : result.getAllErrors()) {
+//				errorList.add(error.getDefaultMessage());
+//			}
+//			redirectAttributes.addFlashAttribute("staff_code", staff_code_before);
+//			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", result);
+//			User user1 = service.selectOne(staff_code_before);
+//			model.addAttribute("user", user1);
+			return "edit";
+		}
 		service.updateOne(staff_code_before, user);
 
 		return "/update";
 	}
 
 	@GetMapping(value = "/delete/{staff_code}")
-	public String displayDelete(Model model, @PathVariable("staff_code") String staffCode) {
+	public String displayDelete(Model model, @PathVariable("staff_code") String staff_ode) {
 
-		User user = service.deleteOne(staffCode);
+		User user = service.deleteOne(staff_ode);
 		model.addAttribute("user", user);
 
 		return "/delete";
